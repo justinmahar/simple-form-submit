bbdfaedaf70448c6ac4e4d9072ad6bd697a8f3d3
Justin Mahar <contact@justinmahar.com>
2024-02-09 17:17:39 -0500

    Run updater

--- dist/index.d.ts
+++
@@ -1,9 +0,0 @@
-import FormData from 'form-data';
-export declare const DEFAULT_METHOD = "POST";
-export declare const submitFormData: (formActionUrl: string, formData: FormData, fetchRequestInit?: RequestInit) => Promise<Response>;
-export declare const submitForm: (formActionUrl: string, form: HTMLFormElement, fetchRequestInit?: RequestInit) => Promise<Response>;
-declare const _default: {
-    submitFormData: (formActionUrl: string, formData: FormData, fetchRequestInit?: RequestInit) => Promise<Response>;
-    submitForm: (formActionUrl: string, form: HTMLFormElement, fetchRequestInit?: RequestInit) => Promise<Response>;
-};
-export default _default;
